4601940338da3d9eadc62f187552db9c92c2be95
Jefferson Nascimento <Jeffersonnascimento6380@gmail.com>
2025-06-12 01:37:34 -0300

    Adicionando pastas do projeto; Adicionando rotas, controllers, e models para a entidade Aluno, e models para o relacionamento do aluno com responsavel e turma

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,14 @@
+//essa pasta api.js contem a rota '/', ela é o inicio de todas as toras do projeto.
+//aperte CONTROL e clique no link depois do caminho '/' e sigua o fluxo.
 import express from 'express';
 import { Router } from 'express';
 
-export default function()
-{
+import alunoApi from './api/alunoApi'
+
+export default (function() {
+    const router = Router()
     
-}
-();
+    //rota para api de aluno
+    router.use('/', alunoApi)
+    
+})();
