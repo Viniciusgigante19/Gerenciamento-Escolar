5077820bf90927ec92be2139037e2f2786c38205
Jefferson Nascimento <jeffersonnascimento6380@gmail.com>
2025-06-17 20:41:52 -0300

    adicionando as pastas de rotas e controlers para a entidade atividades

--- routes/api/alunoApi.js
+++ routes/api/alunoApi.js
@@ -1,7 +1,9 @@
 //esse arquivo de rotas 'alunoApi' contem rotas para o controller, por sua vez tem rotas para as models, que interage com banco de dados
 import { Router } from "express";
-import ListAlunosController from '../../app/controllers/alunoApi/listAlunoController'
-
+import ListAlunosController from '../../app/controllers/alunoApi/listAlunoController.js'
+import GetAlunoController from '../../app/controllers/alunoApi/GetAlunoController.js'
+import InsertAlunoController from '../../app/controllers/alunoApi/insertAlunoController.js'
+import UpdateAlunoController from '../../app/controllers/alunoApi/updateAlunoController.js'
 
 //o link alunoApi importa o codgo abaixo
 //aperte CONTROL e clique no link depois do caminho '/...' e sigua o fluxo.
@@ -9,7 +11,21 @@ export default (function () {
 
     const router = Router();
 
-    //roda api para listar alunos (Get)
+    //rota api para listar alunos (Get)
     router.get('/alunos', ListAlunosController);
+
+    //rota api para obter aluno por RA
+    router.get('/aluno/id', GetAlunoController )
+
+    // rota api para adicionar um aluno
+    router.post('/aluno', InsertAlunoController);
+
+    // rota api para atualizar um aluno
+    router.put('/aluno/:id', UpdateAlunoController);
+
+    // rota api para deletar um aluno
+    router.delete('/aluno/:id', DeleteAlunoController);
+
+    return router;
 
 })();
