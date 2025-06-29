d266c6e25a7c704ee68d44fe5a415f9b6e4b9241
gigante <viniciusgigante935@gmail.com>
2025-06-28 21:57:07 -0300

    Melhorias na estrutura e informações em como usar o swagger em cada rota

--- app/Middlewares/registrarUser.js
+++ app/Middlewares/registrarUser.js
@@ -1,30 +1,31 @@
-import adminModel from "../models/adminModel.js";
-import sequelize from '../../config/sequelize.js';
+import Admin from "../models/adminModel.js";
+import bcrypt from 'bcrypt';
 
-export default async (req, res) => 
-{
-    const { nome ,email, password } = req.body;
-    if (!nome || !email || !password) {
-        return res.status(400).json({ message: 'Todos os campos são obrigatórios.' });
+export default async (req, res) => {
+  const { nome, email, password } = req.body;
+
+  if (!nome || !email || !password) {
+    return res.status(400).json({ message: 'Todos os campos são obrigatórios.' });
+  }
+
+  try {
+    const existingUser = await Admin.findOne({ where: { email } });
+
+    if (existingUser) {
+      return res.status(400).json({ message: 'Usuário já existe.' });
     }
 
-    try {
-        const Admin = adminModel(sequelize);
-        const existingUser = await Admin.findOne({ where: { email } });
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-        if (existingUser) {
-            return res.status(400).json({ message: 'Usuário já existe.' });
-        }
+    const newUser = await Admin.create({
+      nome,
+      email,
+      senha: hashedPassword
+    });
 
-        const newUser = await Admin.create({
-            nome,
-            email,
-            senha: password
-        });
-
-        return res.status(201).json({ message: 'Usuário registrado com sucesso.', user: newUser });
-    } catch (error) {
-        console.error('Erro ao registrar usuário:', error);
-        return res.status(500).json({ message: 'Erro interno do servidor.' });
-    }
-}
+    return res.status(201).json({ message: 'Usuário registrado com sucesso.', user: newUser });
+  } catch (error) {
+    console.error('Erro ao registrar usuário:', error);
+    return res.status(500).json({ message: 'Erro interno do servidor.' });
+  }
+};
